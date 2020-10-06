0e2104515f0056ff9630ca05bffcd16c95ac2f61
nstammewar <nstammewar@gmail.com>
2020-10-06 13:52:46 +0530

    extends class and two interface implement

--- src/interfaceScenarios/methodoverride/Child.java
+++ src/interfaceScenarios/methodoverride/Child.java
@@ -1,6 +1,6 @@
 package interfaceScenarios.methodoverride;
 
-public class Child implements  A,B{
+public class Child extends Parent implements  A,B {
     //1.need to override method when have same method in interfaces that are implemented
     //to reproduce error comment show method in child class
     /*
@@ -9,12 +9,16 @@ public class Child implements  A,B{
 
     public static void main(String[] args) {
         Child child=new Child();
-        child.show();//from Child Class
+        child.show();//from Child Class  if(show method not overridden ) then  From parent Class
+
     }
 
 
     @Override
     public void show() {
+        A.super.show();//From interface A
+        B.super.show();//From interface B
+        super.show();//From parent Class
         System.out.println("from Child Class");
     }
 }
